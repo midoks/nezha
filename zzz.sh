ea88ee526b2cc92ea7c346047fd4d379b5f575fa
midoks <midoks@163.com>
2023-06-25 11:10:56 +0800

    Update zzz.sh

--- zzz.sh
+++ zzz.sh
@@ -2,12 +2,10 @@
 
 goreleaser release --skip-publish --rm-dist --snapshot
 
-cd dist 
-
-mkdir -p tmp
-
-cp -rf nezha-darwin-amd64.zip tmp/nezha-darwin-amd64.zip
-
-cd tmp && unzip nezha-darwin-amd64.zip
-
-./nezha web
+if [ -f dist/nezha-darwin-amd64.zip ];then
+	cd dist
+	mkdir -p tmp
+	cp -rf nezha-darwin-amd64.zip tmp/nezha-darwin-amd64.zip
+	cd tmp && unzip nezha-darwin-amd64.zip
+	./nezha web
+fi
